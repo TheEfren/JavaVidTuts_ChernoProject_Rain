aeac9ae1db1c7ef249a47ed670a4bdf51454cb4c
David COOPERSMITH <dahouse2@gmail.com>
2015-10-04 22:30:16 -0700

    Through Video 55

--- src/com/palmdigital/rain/level/SpawnLevel.java
+++ src/com/palmdigital/rain/level/SpawnLevel.java
@@ -26,6 +26,7 @@ public class SpawnLevel extends Level
 			int w = image.getWidth();
 			int h = image.getHeight();
 			tiles = new Tile[w * h];
+			levelPixels = new int[w * h];
 			image.getRGB(0, 0, w, h, levelPixels, 0, w);
 		}
 		catch(IOException e)
@@ -42,9 +43,9 @@ public class SpawnLevel extends Level
 	{
 		for(int i = 0; i < levelPixels.length; i++)
 		{
-			if(levelPixels[i] == 0xff00) 	tiles[i] = Tile.grass;
-			if(levelPixels[i] == 0xffff00) 	tiles[i] = Tile.flower;
-			if(levelPixels[i] == 0x7f7f00) 	tiles[i] = Tile.rock;
+			if(levelPixels[i] == 0xff00ff00) 	tiles[i] = Tile.grass;
+			if(levelPixels[i] == 0xffffff00) 	tiles[i] = Tile.flower;
+			if(levelPixels[i] == 0xff7f7f00) 	tiles[i] = Tile.rock;
 		}
 		
 	}
