f21327aa42ba2415cc0fbc31b527af6fa8ba7c83
David COOPERSMITH <dahouse2@gmail.com>
2015-10-08 06:28:06 -0700

    Through Video 56

--- src/com/palmdigital/rain/level/Level.java
+++ src/com/palmdigital/rain/level/Level.java
@@ -6,9 +6,9 @@ import com.palmdigital.rain.level.tile.VoidTile;
 
 public class Level 
 {
-	protected Tile[] tiles;
 	protected int width, height;
 	protected int[] tilesInt;
+	protected int[] tiles;
 	
 	// constructor for randomly-generated level
 	public Level(int width, int height)
@@ -41,8 +41,7 @@ public class Level
 	}
 	
 	private void time()
-	{
-		
+	{		
 	}
 	
 	public void render(int xScroll, int yScroll, Screen screen)
@@ -57,24 +56,28 @@ public class Level
 		{
 			for(int x = x0; x < x1; x++)
 			{
-				//getTile(x, y).render(x, y, screen);
-				if(x + y * 16 < 0 || x + y * 16 >= 16*16) 
-				{
-					Tile.voidTile.render(x, y, screen);
-					continue;
-				}
-				
-				tiles[x + y * 16].render(x, y, screen);
+				getTile(x, y).render(x, y, screen);
 			}
 		}
 	}
 	
+
+	// Grass 	= 0xff00ff00
+	// Flower 	= 0xffffff00 
+	// Rock		= 0xff7f7f00
+	
 	public Tile getTile(int x, int y)
 	{
-		if(x < 0 || y < 0 || x >= width || y >= height) return Tile.voidTile;
-		if(tilesInt[x + y * width] == 0) return Tile.grass;
-		if(tilesInt[x + y * width] == 1) return Tile.flower;
-		if(tilesInt[x + y * width] == 2) return Tile.rock;
+		if(x < 0 || y < 0 || x >= width || y >= height) 
+		{			
+			return Tile.voidTile;
+		}
+		if(tiles[x + y * width] == 0xff00ff00) 
+		{
+			return Tile.grass;
+		}
+		if(tiles[x + y * width] == 0xffffff00) return Tile.flower;
+		if(tiles[x + y * width] == 0xff7f7f00) return Tile.rock;
 		return Tile.voidTile;
 	}
 }
--- src/com/palmdigital/rain/level/SpawnLevel.java
+++ src/com/palmdigital/rain/level/SpawnLevel.java
@@ -11,8 +11,6 @@ import com.palmdigital.rain.level.tile.Tile;
 
 public class SpawnLevel extends Level
 {
-	private int[] levelPixels;
-
 	public SpawnLevel(String path) 
 	{
 		super(path);
@@ -25,9 +23,8 @@ public class SpawnLevel extends Level
 			BufferedImage image = ImageIO.read(SpawnLevel.class.getResource(path));
 			int w = image.getWidth();
 			int h = image.getHeight();
-			tiles = new Tile[w * h];
-			levelPixels = new int[w * h];
-			image.getRGB(0, 0, w, h, levelPixels, 0, w);
+			tiles = new int[w * h];
+			image.getRGB(0, 0, w, h, tiles, 0, w);
 		}
 		catch(IOException e)
 		{
@@ -36,17 +33,7 @@ public class SpawnLevel extends Level
 		}
 	}
 
-	// Grass 	= 0xff00
-	// Flower 	= 0xffff00 
-	// Rock		= 0x7f7f00
 	protected void generateLevel()
 	{
-		for(int i = 0; i < levelPixels.length; i++)
-		{
-			if(levelPixels[i] == 0xff00ff00) 	tiles[i] = Tile.grass;
-			if(levelPixels[i] == 0xffffff00) 	tiles[i] = Tile.flower;
-			if(levelPixels[i] == 0xff7f7f00) 	tiles[i] = Tile.rock;
-		}
-		
 	}
 }
