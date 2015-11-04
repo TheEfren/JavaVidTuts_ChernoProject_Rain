5ebdcf5327dc5b709d66903387d5a41b7087b8f5
dcoopersmith <dcoopersmith@PLMS-LAP-ST44.mvusd.k12.ca.us>
2015-11-04 08:42:13 -0800

    A couple changes

--- src/com/palmdigital/rain/entity/projectile/WizardProjectile.java
+++ src/com/palmdigital/rain/entity/projectile/WizardProjectile.java
@@ -9,8 +9,8 @@ public class WizardProjectile extends Projectile
 	public WizardProjectile(int x, int y, double dir) 
 	{
 		super(x, y, dir);
-		range = random.nextInt(100) + 150;
-		speed = 1;
+		range = 200;
+		speed = 4;
 		damage = 20;
 		rateOfFire = 15;
 		sprite = Sprite.projectile_wizard;
