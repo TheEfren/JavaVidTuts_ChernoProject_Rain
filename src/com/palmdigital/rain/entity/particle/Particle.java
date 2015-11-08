e596f743d0866e8fa711eeacfb796b5d790cc6bd
dcoopersmith <dcoopersmith@mvusd.net>
2015-11-07 19:10:37 -0800

    Through Episode 80

--- src/com/palmdigital/rain/entity/particle/Particle.java
+++ src/com/palmdigital/rain/entity/particle/Particle.java
@@ -11,8 +11,10 @@ public class Particle extends Entity
 	private int life;
 	private int time = 0;
 	
-	protected double xx, yy, xa, ya; 	// xa & ya represent the amount of pixels we move on the x and y axes
-										// xx & yy represent 
+	protected double xx, yy, zz; 	// xx & yy represent double version of the x & y ints	 	
+	protected double xa, ya, za;	// xa & ya represent the amount of pixels we move on the x and y axes	
+	
+	
 	
 	public Particle(int x, int y, int life)
 	{
@@ -23,23 +25,35 @@ public class Particle extends Entity
 		this.life = life + (random.nextInt(20) - 10);
 		sprite = Sprite.particle_normal;
 		
-		this.xa = random.nextGaussian();	// random movement in xa and ya
+		this.xa = random.nextGaussian() + 1.8;	// random movement in xa and ya
+		if(this.xa < 0) xa = 0.1;
 		this.ya = random.nextGaussian();
+		this.zz = random.nextFloat() + 2.0;
 	}
 	
 	public void update()
 	{
 		time++;
-		if(time >= 7400) time = 0;
-		
+		if(time >= 7400) time = 0;		
 		if(time > life) remove();
+		za -= 0.1;
+		
+		if(zz <= 0)
+		{
+			zz = 0;
+			za *= -0.55;
+			xa *= 0.4;
+			ya *= 0.4;
+		}
+		
 		
 		this.xx += xa;
-		this.yy += ya;		
+		this.yy += ya;	
+		this.zz += za;
 	}
 	
 	public void render(Screen screen)
 	{
-		screen.renderSprite((int)xx, (int)yy, sprite, true);
+		screen.renderSprite((int)xx - 12, (int)yy - (int)zz, sprite, true);
 	}
 }
