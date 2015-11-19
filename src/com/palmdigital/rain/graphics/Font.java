740da20175380fe449cab80b6c395e7d99666b79
dcoopersmith <dcoopersmith@mvusd.net>
2015-11-18 18:47:04 -0800

    Through Episode 110

--- src/com/palmdigital/rain/graphics/Font.java
+++ src/com/palmdigital/rain/graphics/Font.java
@@ -4,14 +4,29 @@ public class Font
 {
 	private static SpriteSheet font = new SpriteSheet("/fonts/arial.png", 16);
 	private static Sprite[] characters = Sprite.split(font);
+	
+	private static String charIndex = 	"ABCDEFGHIJKLM" + //
+										"NOPQRSTUVWXYZ" + //
+										"abcdefghijklm" + //
+										"nopqrstuvwxyz" + //
+										"0123456789.,'" + //
+										"'\"\";:!@$%()-+"; //
 			
 	public Font()
 	{
 		
 	}
 	
-	public void render(Screen screen)
+	public void render(String text, Screen screen)
 	{
-		screen.renderSprite(50, 50, characters[1], false);
+		int x = 10;
+		int y = 50;
+		for(int i = 0; i < text.length(); i++)
+		{
+			char currentChar = text.charAt(i);
+			int index = charIndex.indexOf(currentChar);
+			if(index == -1) continue;
+			screen.renderSprite(x + i * 14, y, characters[index], false);
+		}
 	}
 }
